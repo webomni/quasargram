7990878772a78b4d45ddd9843401c37bc1e97bd9
webomni <prpaulohcmg@hotmail.com>
2023-11-06 07:30:36 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          to="/camera"
+          flat
+          round
+          color="primary"
+          icon="eva-camera-outline"
+          size="18px"
+          dense
+          class="large-screen-only q-mr-sm"
+        ></q-btn>
+        <q-separator vertical spaced class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+        <q-btn
+          to="/"
+          flat
+          round
+          color="primary"
+          icon="eva-home-outline"
+          size="18px"
+          dense
+          class="large-screen-only"
+        ></q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="scss">
+.q-toolbar {
+  @media (min-width: $breakpoint-sm-min) {
+    height: 77px;
+  }
+}
+.q-footer .q-tab__icon {
+  font-size: 30px;
+}
+.q-toolbar__title {
+  font-size: 30px;
+  @media (max-width: $breakpoint-xs-max) {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page class="constrain q-pa-md">
+    <q-card class="my-card" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Title</q-item-label>
+          <q-item-label caption> Subhead </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section class="col-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHome",
+});
+</script>
