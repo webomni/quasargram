75f80b4333a6c004cd057576a1002e2a8c836161
pauloh <ph@sygmasistemas.com.br>
2023-11-06 17:57:53 -0300

    Backup 06 11 23 1800

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,55 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Camera</h5>
+  <q-page class="constrain-more q-pa-md" id="PageCamera">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="black" icon="eva-camera" size="lg" />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          label="Caption"
+          class="col col-sm-6"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          label="Location"
+          class="col col-sm-6"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn rounded color="primary" unelevated label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { uid } from "quasar";
+import { reactive } from "vue";
 
-export default defineComponent({
-  name: "PageCamera",
-});
+const post = reactive([
+  {
+    id: uid(),
+    caption: "",
+    location: "",
+    photo: null,
+    date: Date.now(),
+  },
+]);
 </script>
+
+<style lang="scss" scoped>
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,40 +1,97 @@
 <template>
-  <q-page class="constrain q-pa-md">
-    <q-card class="my-card" flat bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-        </q-item-section>
+  <q-page class="constrain q-pa-md" id="PageHome">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img
+                  src="https://gravatar.com/userimage/92146302/2e776184dad15bb4feb614efd20b7fca.jpeg?size=256"
+                />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label>Title</q-item-label>
-          <q-item-label caption> Subhead </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">paulo_castro</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
+          <q-img :src="post.imageURL" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <!-- min profile -->
 
-      <q-card-section horizontal>
-        <q-card-section>
-          {{ lorem }}
-        </q-card-section>
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img
+                src="https://gravatar.com/userimage/92146302/2e776184dad15bb4feb614efd20b7fca.jpeg?size=256"
+              />
+            </q-avatar>
+          </q-item-section>
 
-        <q-separator vertical />
-
-        <q-card-section class="col-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </q-card-section>
-      </q-card-section>
-    </q-card>
+          <q-item-section>
+            <q-item-label class="text-bold">paulo_castro</q-item-label>
+            <q-item-label caption> Dannyj </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { reactive } from "vue";
+import { date } from "quasar";
 
-export default defineComponent({
-  name: "PageHome",
-});
+const posts = reactive([
+  {
+    id: 1,
+    caption: "Gold gate Bridge",
+    date: date.formatDate(1699295959089, "MMMM D h:mmA"),
+    location: "San Francisco, United States",
+    imageURL: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 2,
+    caption: "Gold gate Bridge",
+    date: date.formatDate(1699295959089, "MMMM D h:mmA"),
+    location: "San Francisco, United States",
+    imageURL: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 3,
+    caption: "Gold gate Bridge",
+    date: date.formatDate(1699295959089, "MMMM D h:mmA"),
+    location: "San Francisco, United States",
+    imageURL: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+  {
+    id: 4,
+    caption: "Gold gate Bridge",
+    date: date.formatDate(1699295959089, "MMMM D h:mmA"),
+    location: "Monte Carmelo - MG",
+    imageURL: "https://cdn.quasar.dev/img/parallax2.jpg",
+  },
+]);
 </script>
+
+<style lang="scss" scoped>
+.card-post .q-img {
+  min-height: 200px;
+}
+</style>
